b7a5e00cf556da078c4c63a80ec3fbaa3dea1d2a
Baek.yj <dudwls96@gmail.com>
2024-05-15 21:50:44 +0900

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# Backstage Tutorial
+
+1. Backstage란 무엇인가?
+
+2. Backstage 테스트를 위한 로컬 개발 환경 구성하기
+
+3. Backstage 와 Gitea 연동하기
+
+4. Backstage 와 Gitea Actions 활용한 CI/CD 자동화 환경 만들기
+
+5. Backstage 와 Terraform 연동 및 LocalStack 활용한 테스트 환경 만들기
--- scripts/install_argocd_on_k3.sh
+++ scripts/install_argocd_on_k3.sh
@@ -16,3 +16,11 @@ kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "NodePort"}}'
 #kubectl get all -n argocd
 kubectl get svc -n argocd
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+
+
+# ArgoCD CLI
+ARGOCD_VERSION=$(curl --silent "https://api.github.com/repos/argoproj/argo-cd/releases/latest" | grep '"tag_name"' | sed -E 's/.*"([^"]+)".*/\1/')
+curl -sSL -o ./argocd-${ARGOCD_VERSION} https://github.com/argoproj/argo-cd/releases/download/${ARGOCD_VERSION}/argocd-linux-amd64
+chmod +x ./argocd-${ARGOCD_VERSION}
+sudo mv ./argocd-${ARGOCD_VERSION} /usr/local/bin/argocd 
+argocd version --client
